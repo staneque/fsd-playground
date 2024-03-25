e5d879ad2f4d27031dec7cd3e2aa13c27e93e3d2
Stanislav Ovcharov <stanislav@juniqe.com>
2024-03-25 22:03:58 +0100

    add sidebar story

--- src/shared/config/storybook/ThemeDecorator/index.tsx
+++ src/shared/config/storybook/ThemeDecorator/index.tsx
@@ -1,8 +1,12 @@
 import { StoryFn } from '@storybook/react'
 import { Theme } from 'app/providers/ThemeProvider'
 
-export const ThemeDecorator = (theme: Theme) => (Story: StoryFn) => (
-  <div className={`app ${theme}`} style={{ height: 'auto', padding: '100px' }}>
-    <Story />
-  </div>
-)
+export const ThemeDecorator =
+  (theme: Theme, fullHeight: boolean) => (Story: StoryFn) => (
+    <div
+      className={`app ${theme}`}
+      style={{ height: fullHeight ? '100vh' : 'auto', padding: '50px' }}
+    >
+      <Story />
+    </div>
+  )
---
+++ src/widgets/Sidebar/ui/Sidebar.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Sidebar } from 'widgets/Sidebar'
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator'
+import { Theme } from 'app/providers/ThemeProvider'
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Widgets/Sidebar',
+  component: Sidebar,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {},
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: {},
+} satisfies Meta<typeof Sidebar>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const LightTheme: Story = {
+  decorators: [ThemeDecorator(Theme.LIGHT, true)],
+}
+
+export const DarkTheme: Story = {
+  decorators: [ThemeDecorator(Theme.DARK, true)],
+}
