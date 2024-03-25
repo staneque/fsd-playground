2c94ce6fcdb2b4de5b7d79c40ef8777f8bced113
Stanislav Ovcharov <stanislav@juniqe.com>
2024-03-26 00:34:51 +0100

    fix names noop

--- src/widgets/navbar/ui/Navabar.stories.tsx
+++ src/widgets/navbar/ui/Navabar.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Navbar } from 'widgets/Navbar'
-import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorato'
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator'
 import { Theme } from 'app/providers/ThemeProvider'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
